af7d864597112eed80c4d6e3ea2e4c52310051c2
koapic1 <koapic1@gmail.com>
2021-12-23 14:25:44 +0900

    가비지 컬렉션

--- src/lecture/MethodOverloadingTest.java
+++ src/lecture/MethodOverloadingTest.java
@@ -10,5 +10,10 @@ public class MethodOverloadingTest {
 		System.out.println(sum02);
 		double sum03 =  mo.getSum(10.5, 11.4);
 		System.out.println(sum03);
+		Circle aa = new Circle();
+		Circle bb = new Circle();
+		bb = aa; // 객체의 할당이나 null은 쓰레기가 된다.
+		//bb는 없어도 되는 것 즉 가비지 컬렉션의 수거 대상이 된다.
+		//여기서 중요한건 수거 대상이 된다는거지 즉시 사라지거나 그렇진 않다.
 	}
 }
